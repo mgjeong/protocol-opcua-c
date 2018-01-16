9be28440116da05d0e139bfd2f48728b3d05049a
서지환/IoT Lab(SR)/Senior Engineer/삼성전자 <jihwan.seo@samsung.com>
2018-01-16 19:19:02 +0900

    modify unittest shell (#35)

--- unittest.sh
+++ unittest.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 echo "Start edge opcua build"
+PROJECT_ROOT=$(pwd)
+echo $PROJECT_ROOT
 
+cd $PROJECT_ROOT
+scons TEST=1
+cd ./unittests
+./prep.sh
 scons TEST=1
 
 echo "End of edge opcua build"
