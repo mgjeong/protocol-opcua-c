313f043cca44535191e09fbbe4e525f059b0d4c0
ChaJiWon <jw_wonny.cha@samsung.com>
2018-04-18 21:36:19 +0900

    Add install shell script
    - install.sh : copy shared library & header file to user area

---
+++ install.sh
@@ -0,0 +1,30 @@
+###############################################################################
+# Copyright 2018 Samsung Electronics All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+###############################################################################
+
+#!/bin/bash
+
+USR_LIB_DIR=/usr/local/lib
+USR_INC_DIR=/usr/local/include
+KEYWORD_OPCUA_C=opcua-c
+
+# COPY shared library to /usr/local/lib
+sudo cp -f ./build/libopcua-adapter.so $USR_LIB_DIR
+
+# COPY header file to /usr/local/include/opcua-c
+sudo rm -rf $USR_INC_DIR/$KEYWORD_OPCUA_C
+sudo mkdir $USR_INC_DIR/$KEYWORD_OPCUA_C
+sudo cp -f ./include/* $USR_INC_DIR/$KEYWORD_OPCUA_C/
